6c0eb6cf68acef1c47ee5a06fe19b11719f119e3
Denis228007 <denisremix2006@gmail.com>
2025-09-16 17:23:48 +0000

    midl+

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/middleware-test', function () {
+    return "Ти пройшов middleware ✅";
+})->middleware('check.word');
+
